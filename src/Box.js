548fdeef536bff544b9cc35a9df2b9c6e83fca9a
Chris Aquino <chris.aquino@gmail.com>
2018-07-19 10:02:11 -0400

    completes the small exercises, phase 3

--- src/Box.js
+++ src/Box.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+function printMessage() {
+  console.log("Hello from the box!");
+}
+
 const Box = (props) => {
   return (
     <div className="box">
       {props.message}
+      <button onClick={printMessage}>click me</button>
     </div>
   )
 }
